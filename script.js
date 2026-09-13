7577d1f210db62142d90ab1594eda783c1ea25bd
Suyi <shouyirs@gmail.com>
2026-09-13 20:04:51 +0100

    Actualizo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let errores = 0;
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const submitBtn = document.getElementById("submitBtn");
@@ -9,12 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
         const code = codeInput.value.trim();
 
         if (code === "Melocotón") {
-            message.textContent = "MUY BIEEEN JAJAJAJ.<br>Dale, a por el siguiente.";
+            message.innerHTML = "MUY BIEEEN JAJAJAJ.<br>Dale, a por el siguiente.";
         } else {
-            message.textContent = "Ese gato no era";
+            errores++;
+
+            if (errores === 1) {
+                message.innerHTML = "No ese no, otro...";
+            } else if (errores === 2) {
+                message.innerHTML = "Ummmmm ese tampoco eh jsjsj";
+            } else {
+                message.innerHTML = "Solo tienes 4 gatos, piensa...";
+            }
         }
     });
-
+            
     resetBtn.addEventListener("click", () => {
         codeInput.value = "";
         message.textContent = "";
